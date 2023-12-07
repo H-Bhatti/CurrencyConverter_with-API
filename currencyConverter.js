80791636b74b0b2f73ba09d0a8b1e99b0535996b
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2023-12-07 22:41:14 +0400

    add first

---
+++ currencyConverter.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+//  for running code in html and node envioment
+
+
+// Function for gettting the currency conversation rates
+
+
+const conversionRate = async(fromCurrency, toCurrency)=>{
+    const response = await axios.get("https://v6.exchangerate-api.com/v6/722076cb548fbaa301bc549e/latest/USD");
+    const rates = response.data.conversion_rates;       // only get the rates wihtout the additional Data
+    const usd = parseFloat(1/rates[fromCurrency]);           // gets the curency conversion rate/USD for selected currency
+    console.log(usd)
+    const exchangedAmount = parseFloat(usd * rates[toCurrency])
+    return exchangedAmount;
+}
+// function for getting the ful country names
+
+
+const getCoutries = async (toCurrency) =>{
+    var cont = require("./currencies.json")
+
+    console.log(cont[toCurrency]["symbolNative"])
+    console.log(cont[toCurrency]["name"])
+
+}
+
+
+
+// conversionRate("PKR","PKR");
+getCoutries("PKR")
