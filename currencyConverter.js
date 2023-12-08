69f61b399b5ba523aff2db57b0ce0526b8d9d82e
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2023-12-08 22:57:18 +0400

    Backend completed need to add front end

--- currencyConverter.js
+++ currencyConverter.js
@@ -9,25 +9,41 @@ const conversionRate = async(fromCurrency, toCurrency)=>{
     const response = await axios.get("https://v6.exchangerate-api.com/v6/722076cb548fbaa301bc549e/latest/USD");
     const rates = response.data.conversion_rates;       // only get the rates wihtout the additional Data
     const usd = parseFloat(1/rates[fromCurrency]);           // gets the curency conversion rate/USD for selected currency
-    console.log(usd)
     const exchangedAmount = parseFloat(usd * rates[toCurrency])
+
+    if(isNaN(exchangedAmount)){
+        throw new Error(`Unable to get ${fromCurrency} and ${toCurrency}`);
+    }
     return exchangedAmount;
 }
-// function for getting the ful country names
+// function for getting the ful country names from local JSON file
 
 
-const getCoutries = async (toCurrency) =>{
-    var cont = require("./currencies.json")
-
-    console.log(cont[toCurrency]["symbolNative"])
-    console.log(cont[toCurrency]["name"])
-
+const getCoutries = (toCurrency) =>{
+    var data = require("./currencies.json")
+    if(data[toCurrency] == undefined){
+        throw new Error (`unable to get currency data for  ${toCurrency}`)
+    }
+    return [data[toCurrency]["symbolNative"], data[toCurrency]["name"]]
 }
 
 
 // now need to add ocde that checks and adds the currency symbol to the conversion and tells the name of cuntries
 // multiply the toal amount and calling both function need to make 
+const convert = async (fromCurrency, toCurrency, ammount)=>{
 
+    const rate = await conversionRate (fromCurrency, toCurrency);
+    const symbol = getCoutries(toCurrency)
+    const total = (rate * ammount).toFixed(3)
+    // console.log(total+" "+symbol[0]," ",symbol[1])
+    return `${total} ${symbol[0]}, ${symbol[1]}`;
+
+}
 
 // conversionRate("PKR","PKR");
-getCoutries("EUR")
+convert("USD", "EUR", 10)
+.then((message)=>{
+    console.log(message)})
+.catch((error)=>{
+    console.error(error)
+})
